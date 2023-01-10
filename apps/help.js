a61a9679823dd7e46ff1d55ef185ebc9d1936da6
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-10 01:08:49 +0000

    增加帮助(虽然是文字版)
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

---
+++ apps/help.js
@@ -0,0 +1,31 @@
+
+// 欢迎各位大佬萌新进群玩：657142904
+import plugin from "../../../lib/plugins/plugin.js"
+import { segment } from "oicq";
+
+export class help extends plugin {
+    constructor() {
+		super({
+			name: '脆脆鲨帮助',
+			dsc: '0v0v0',
+			/** https://oicqjs.github.io/oicq/#events */
+			event: 'message',
+			priority: 1000,
+			rule: [
+                {
+                    /** 命令正则匹配 */
+                    reg: '^#?脆脆鲨?帮助|菜单$',
+                    /** 执行方法 */
+                    fnc: 'Marenhelp'
+                }
+            ]
+        })
+    }
+
+    async Marenhelp(e) {
+
+        await e.reply('脆脆鲨帮助(文字版)\n#本群(禁用|启用)自动撤回\n#打我使用说明\n#骂人使用说明\n#禁用功能(施工中...)\n#脆脆鲨更新|脆脆鲨强制更新')
+        return true  
+    }
+}
+
