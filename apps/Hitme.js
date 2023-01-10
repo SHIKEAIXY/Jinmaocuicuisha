801f10cde91857d683b8a28da18bb93472d915d8
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-10 23:39:00 +0000

    update apps/Hitme.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Hitme.js
+++ apps/Hitme.js
@@ -128,8 +128,7 @@ async setHitMeapi(e){
         if (e.msg.includes('删除打我api')){
         let data=await getread()
         if (!data.length > 0) { return  e.reply('api都没添加，你删毛线呢？') }
-        let ht=data[num-1]
-        await data.splice(data.indexOf(ht), 1)
+        await data.splice(data.indexOf(data), 1)
         await getwrite(data)
         await e.reply(`删除成功~`)
         return true
