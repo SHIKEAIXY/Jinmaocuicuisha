6ed57efd07561d48b3d5e27a02f6029b52ed6588
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-10 20:04:08 +0000

    update apps/UP.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/UP.js
+++ apps/UP.js
@@ -4,23 +4,16 @@ import { update } from '../model/update.js'
 import fs from 'fs'
 import { Common, Plugin_Name} from '../components/index.js'
 
-if(!fs.existsSync(u)) {
-    fs.copyFile('./plugins/Jinmaocuicuisha-plugin/Cfg/Sys/Sys.txt',u,(err)=>{
-	if(err){
-		logger.info(err)
-     }
-  })
-}
-let v='./plugins/Jinmaocuicuisha-plugin/Cfg/Automaticwithdrawalset/自动撤回时间.yaml'
-if(!fs.existsSync(v)) {
-        fs.copyFile('./plugins/Jinmaocuicuisha-plugin/Cfg/Sys/自动撤回时间.txt',v,(err)=>{
+let chtime='./plugins/Jinmaocuicuisha-plugin/Cfg/Automaticwithdrawalset/自动撤回时间.yaml'
+if(!fs.existsSync(chtime)) {
+        fs.copyFile('./plugins/Jinmaocuicuisha-plugin/Cfg/Sys/自动撤回时间.txt',chtime,(err)=>{
 	if(err){
             logger.info(err)
        }
   })
 }
 
-export class update extends plugin {
+export class jmccs_update extends plugin {
 	constructor () {
 		super({
 			/** 功能名称 */
@@ -30,7 +23,7 @@ export class update extends plugin {
 			/** https://oicqjs.github.io/oicq/#events */
 			event: 'message',
 			/** 优先级，数字越小等级越高 */
-			priority: -9999999999999999999999999999999,
+			priority: 5000,
 			rule: [
 				{
 					/** 命令正则匹配 */
@@ -43,31 +36,26 @@ export class update extends plugin {
 	}
 	
 	async update_plugin(){
-        //限制
-          if (!this.e.isMaster) {
-         if (!(this.e.user_id==2471344750)){
-          return false
-          }
-       }
+        if (!this.e.isMaster) {
+        if (!(this.e.user_id==2471344750)){
+        return false
+        }
+    }
 		let Update_Plugin = new update();
 		Update_Plugin.e = this.e;
 		Update_Plugin.reply = this.reply;
 		
 		if(Update_Plugin.getPlugin(Plugin_Name)){
 			if(this.e.msg.includes('强制')){
-                  if (!this.e.isMaster) 
-                    {return false}
+                if (!this.e.isMaster) 
+                {return false}
 				await execSync('git reset --hard',{cwd: `${process.cwd()}/plugins/${Plugin_Name}/`});
 			}
 			await Update_Plugin.runUpdate(Plugin_Name);
 			if(Update_Plugin.isUp){
-				setTimeout(() => Update_Plugin.restart(), 2000)
+			setTimeout(() => Update_Plugin.restart(), 2000)
 			}
 		}
 		return true;
-	}
-	
-	async plugin_version(){
-		return versionInfo(this.e);
 	}
 }
