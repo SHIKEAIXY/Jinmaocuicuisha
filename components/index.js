f27372001e47e5e2f91f4c6c50a8082e49ccc861
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-10 18:36:28 +0000

    up
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

---
+++ components/index.js
@@ -0,0 +1,8 @@
+const Path = process.cwd();
+const Plugin_Name = 'Jinmaocuicuisha-plugin'
+const Plugin_Path = `${Path}/plugins/${Plugin_Name}`;
+import Data from './Data.js'
+import Common from './Common.js'
+import Config from './Config.js'
+
+export {Common, Config, Data, Path, Plugin_Name, Plugin_Path}
