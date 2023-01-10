e066c069e64919dcd86f3f6728c7448b1c8723f1
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-10 18:39:08 +0000

    update components/index.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- components/index.js
+++ components/index.js
@@ -3,6 +3,6 @@ const Plugin_Name = 'Jinmaocuicuisha-plugin'
 const Plugin_Path = `${Path}/plugins/${Plugin_Name}`;
 import Data from './Data.js'
 import Common from './Common.js'
-import Config from './Config.js'
+import Config from './Cfg.js'
 
-export {Common, Config, Data, Path, Plugin_Name, Plugin_Path}
+export {Common, Cfg, Data, Path, Plugin_Name, Plugin_Path}
