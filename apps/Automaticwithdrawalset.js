e1f611f0d89b32ea00cf5fcb3cad230fe6354fbc
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-10 16:04:00 +0000

    update apps/Automaticwithdrawalset.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Automaticwithdrawalset.js
+++ apps/Automaticwithdrawalset.js
@@ -12,8 +12,8 @@ import Yaml from '../Yaml/Yaml.js'
 import fs from 'fs'
 const _path = process.cwd();
 
-let path1='./plugins/Jinmaocuicuisha-plugin/Cfg/Automaticwithdrawalset/qq.yaml'
-if (!fs.existsSync(path1)) {fs.writeFileSync(path1,'')}
+let path='./plugins/Jinmaocuicuisha-plugin/Cfg/Automaticwithdrawalset/qq.yaml'
+if (!fs.existsSync(path)) {fs.writeFileSync(path,'')}
 
 export class Automaticwithdrawalset extends plugin {
     constructor() {
@@ -37,44 +37,18 @@ export class Automaticwithdrawalset extends plugin {
 
     async Automaticwithdrawalset(e) {
 
-   if (!e.isGroup) return false;
+    if (!e.isGroup) return false;
  
-   let data=await getread()
-       if (!data) data= [];
-       if (data.indexOf(e.group_id) == -1&&e.msg.includes('禁用')){
-       await data.push(e.group_id)
-       await e.reply('本群已禁用自动撤回功能~')
-       }
-       if (data.indexOf(e.group_id)!== -1&&e.msg.includes('启用')){
-       await data.splice(data.indexOf(e.group_id), 1)//位置索引，删除1个
-       await e.reply('本群已启用自动撤回功能~')
-       }
-       getwrite(data)
+      let data=await Yaml.getread(path)
+      if (!data) data= [];
+      if (data.indexOf(e.group_id) == -1&&e.msg.includes('禁用')){
+      await data.push(e.group_id)
+      await e.reply(`本群已禁用自动撤回~`)
+      }
+      if (data.indexOf(e.group_id)!== -1&&e.msg.includes('启用')){
+      await data.splice(data.indexOf(e.group_id), 1)
+      await e.reply(`本群已启用自动撤回~`)
+      }
+      Yaml.getwrite(path,data)
     }
 }
-
-    /** 读取 */
-    function getread() {
-    try {
-        var fileContents = fs.readFileSync(path1, 'utf8');
-        } catch (e) {
-        console.log(e);
-        return false;
-        }
-        //转换
-        return YAML.parse(fileContents);
-        }
-
-    /** 写入 */
-    function getwrite(data) {
-    try {
-        //转换
-        let yaml = YAML.stringify(data);
-        fs.writeFileSync(path1, yaml, 'utf8');
-        return true
-        } catch (e) {
-        //错误处理
-        console.log(e);
-        return false
-        }
-     }
