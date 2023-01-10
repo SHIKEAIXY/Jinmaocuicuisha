a89255f45bdf5de5ee5c3c3a17eb05be071bd031
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-10 17:52:05 +0000

    update apps/Automaticwithdrawalset.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Automaticwithdrawalset.js
+++ apps/Automaticwithdrawalset.js
@@ -13,6 +13,8 @@ import fs from 'fs'
 const _path = process.cwd();
 
 let path='./plugins/Jinmaocuicuisha-plugin/Cfg/Automaticwithdrawalset/qq.yaml'
+let source={}
+
 if (!fs.existsSync(path)) {fs.writeFileSync(path,'')}
 
 export class Automaticwithdrawalset extends plugin {
@@ -24,6 +26,11 @@ export class Automaticwithdrawalset extends plugin {
 			event: 'message',
 			priority: 5000,
 			rule: [
+                {   /** 命令正则匹配 */
+                    reg: '^#?设置自动撤回时间(\\d)+(秒)?$',
+                    /** 执行方法 */
+                    fnc: 'recalltime',
+                },
                 {
                     /** 命令正则匹配 */
                     reg: '^#?本群(禁用|启用)自动撤回$',
@@ -33,6 +40,18 @@ export class Automaticwithdrawalset extends plugin {
                 }
             ]
         })
+    }
+
+    async recalltime(e){
+
+    if (!e.isMaster)  {return false}
+    let data=await Yaml.getread(path)
+    let recalltime=e.msg.replace(/#|设置自动撤回时间|秒/g,'')
+    let 时间=recalltime
+    recalltime=recalltime*60
+    data.自动撤回时间=recalltime
+    await Yaml.getwrite(path,data)
+    return e.reply(`自动撤回时间,成功设置为${时间}秒`)
     }
 
     async Automaticwithdrawalset(e) {
