9337f4236ae444647d76f4c3db56daf010382364
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-11 16:42:24 +0000

    增加骰子功能
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

---
+++ apps/Dice.js
@@ -0,0 +1,37 @@
+import plugin from '../../lib/plugins/plugin.js'
+import lodash from 'lodash'
+
+export class dice extends plugin {
+  constructor () {
+    super({
+      name: 'roll',
+      dsc: 'roll骰子',
+      event: 'message',
+      priority: 5000,
+      rule: [
+        {
+          reg: '^#?roll|骰子 ',
+          fnc: 'roll'
+        },
+        {
+          reg: '^#?r ',
+          fnc: 'r'
+        }
+      ]
+    })
+  }
+
+  async roll () {
+    const choices = this.e.msg.split(' ').slice(1)
+    const result = lodash.sample(choices)
+    await this.reply(`bot帮你选择：${result}`, false, { at: true })
+  }
+
+  async r () {
+    const range = this.e.msg.split(' ').map(Number).filter(Number.isInteger)
+    const end = range.pop() ?? 100
+    const start = range.pop() ?? 1
+    const result = lodash.random(start, end)
+    await this.reply(`在${start}和${end}间roll到了：${result}`)
+  }
+}
