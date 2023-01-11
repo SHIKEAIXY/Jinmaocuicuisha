6b88deea5c6df0347fae40d541dba221ce0141a3
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-11 19:20:06 +0000

    update apps/Automaticwithdrawal.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Automaticwithdrawal.js
+++ apps/Automaticwithdrawal.js
@@ -46,13 +46,14 @@ export class Automaticwithdrawal extends plugin {
     await Yaml.getwrite(bot, cfg) //重新保存bot.yaml
 
     if (e.user_id == Bot.uin) {
-    let cfg = await Yaml.getread(bot) //加载bot.yaml
-    cfg.ignore_self = true; //开启过滤自己消息
-    await Yaml.getwrite(bot, cfg) //重新保存bot.yaml
-
     let Cfgtime = await Yaml.getread(rectime)
     let 时间 = Cfgtime.自动撤回时间
     let J = (await e.group.getChatHistory(e.My_message, 1))[0].message_id
+
+        let cfg = await Yaml.getread(bot) //加载bot.yaml
+        cfg.ignore_self = true; //开启过滤自己消息
+        await Yaml.getwrite(bot, cfg) //重新保存bot.yaml
+
         await common.sleep(时间);
         await e.group.recallMsg(J);
         return true;
