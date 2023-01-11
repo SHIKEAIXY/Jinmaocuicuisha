de6927b3a8e1148f21fc35f0a1ad922c1695f096
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-11 10:57:02 +0000

    update components/Data.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- components/Data.js
+++ components/Data.js
@@ -1,6 +1,7 @@
 import lodash from 'lodash'
 import fs from 'fs'
 
+const tp_path = process.cwd()
 const _path = process.cwd()
 const plugin = 'Jinmaocuicuisha-plugin'
 const getRoot = (root = '') => {
