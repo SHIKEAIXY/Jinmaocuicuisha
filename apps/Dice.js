b888da233b6b6955225db50a6806c25ddf530209
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-12 10:41:47 +0000

    update apps/Dice.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Dice.js
+++ apps/Dice.js
@@ -2,7 +2,6 @@ import plugin from '../../../lib/plugins/plugin.js'
 import co from '../../../lib/common/common.js'
 import common from'../../../lib/common/common.js'
 import { Cfg } from '../components/index.js'
-import { Dice } from '../model/Dice.js'
 import { segment } from "oicq";
 
 const images = process.cwd() + '/plugins/Jinmaocuicuisha-plugin/Resources/img/Dice/';
