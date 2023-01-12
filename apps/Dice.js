c031730aa7fdd200eb28cf2425a249fc61444a8c
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-12 09:21:23 +0000

    update apps/Dice.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Dice.js
+++ apps/Dice.js
@@ -59,7 +59,7 @@ export class dice extends plugin {
     let msg = e.reply(segment.image(`file:///${images}/0.gif`));
     await common.sleep(500);
     await e.reply(`\n请在【一】到【六】之间选一个数,然后发送【开】来查看结果！`);
-    let roll = (await e.group.setChatHistory(e.msg.seq, 1))[0].message_id
+    await redis.set(`dice:k:${e.message_id}`,msg)
     return true;
     };
 
@@ -67,7 +67,8 @@ export class dice extends plugin {
 
     if (!e.isGroup) return false;
     if(开){
-        let roll = (await e.group.getChatHistory(e.msg.seq, 1))[0].message_id
+        let msgs = await redis.get(`dice:k:${e.message_id}`)
+        let roll = (await e.group.getChatHistory(msgs.seq, 1))[0].message_id
         await e.group.recallMsg(roll);
         let k = Math.ceil(Math.random()*6);
         let 一 = 1
