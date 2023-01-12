17f783749ea32c23c2663d8c7ac64da099ebb588
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-12 23:03:21 +0000

    update apps/Featuredisabled.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Featuredisabled.js
+++ apps/Featuredisabled.js
@@ -30,10 +30,6 @@ export class Featuredisabled extends plugin {
                 {
                     reg: '^#?全局(禁用|启用).*$',
                     fnc: 'Ftdall',
-                },
-                {
-                    reg: '^#?本群(禁用|启用).*$',
-                    fnc: 'Ftdgroup',
                 }
             ],
         });
@@ -94,38 +90,6 @@ export class Featuredisabled extends plugin {
     let 主人权限 = mst.绝对主人;
     if (!(this.e.user_id==主人权限)){e.reply(`你没有权限！`);return false;}
 
-        let data = await Yaml.getread(禁用);
-        let All = data.default.disable;
-        if(All==null){All=[]};
-            if(e.msg.includes('全局禁用')){
-                let 功能 = e.msg.replace(/#|全局禁用|启用/g,'');
-                if(功能==All){return e.reply('该功能已禁用！请不要重复操作！')};
-                if(!功能){return e.reply('要全局禁用的功能呢？')};
-                All.push(功能);
-                await Yaml.getwrite(禁用, data);
-                let msgres = [segment.at(e.user_id), `【${e.msg}】已成功禁用！如要查看全局禁用功能可以使用指令【全局禁用列表】来查看哦~(如果禁用失败请重启试试)`];
-                await e.reply(msgres);
-                return false;
-            }
-
-            if(e.msg.includes('全局启用')){
-            let num = e.msg.match(/\d+/)
-                if (!num) {
-                return  e.reply('序号呢？不知道的话可以通过指令【全局禁用列表】来查看哦~')
-                }
-                let 恢复=All[num-1]
-                if(!恢复){return e.reply('请使用指令【全局禁用列表】，检查序号是否正确，或者检查【全局禁用列表】是否是空的！')}
-                await All.splice(All.indexOf(恢复), 1)
-                await Yaml.getwrite(禁用,data)
-                await e.reply('该功能已成功启用~(如果启用失败请重启试试)')
-            }
-    }
-
-    async Ftdgrouplb (e) {
-
-    if (!e.isMaster) {e.reply(`你没有权限！`);return false;}
-    if (!e.isGroup) {e.reply(`请在要禁用功能的群里使用！`);return false;}
-
     let nickname = Bot.nickname
     if (this.e.isGroup) {
       let info = await Bot.getGroupMemberInfo(this.e.group_id, Bot.uin)
@@ -138,17 +102,17 @@ export class Featuredisabled extends plugin {
     let forwardMsg = [
       {
         ...userInfo,
-        message: '本群已禁用的功能\n可使用【本群启用+(序号)】启用对应功能'
+        message: '全局已禁用的功能\n可使用【全局启用+(序号)】启用对应功能'
       }
     ]
-    let data=await Yaml.getread(禁用)
-    let uid = e.group_id
-    let group = data.uid.disable;
+
+    let data = await readPluginNames(directory);
+    let All = data;
     let msg=[]
-    logger.info(group)
-    if(group==null||group.length==0){return e.reply('本群没有禁用功能呢~')}
-    for (let v = 0; v < group.length; v++) {
-      msg.push(`${v+1}.`+group[v]+'\n')
+    logger.info(All)
+    if(All==null||All.length==0){return e.reply('没有禁用功能呢~')}
+    for (let v = 0; v < All.length; v++) {
+      msg.push(`${v+1}.`+All[v]+'\n')
     }
     forwardMsg.push(
           {
@@ -164,42 +128,18 @@ export class Featuredisabled extends plugin {
     forwardMsg.data = forwardMsg.data
       .replace(/\n/g, '')
       .replace(/<title color="#777777" size="26">(.+?)<\/title>/g, '___')
-      .replace(/___+/, `<title color="#777777" size="26">本群已禁用的功能</title>`)
+      .replace(/___+/, `<title color="#777777" size="26">全局已禁用的功能</title>`)
     return e.reply(forwardMsg)
     }
 
-    async Ftdgroup(e) {
-
-    if (!e.isMaster) {e.reply(`你没有权限！`);return false;}
-    if (!e.isGroup) {e.reply(`请在要禁用功能的群里使用！`);return false;}
-
-        let data = await Yaml.getread(禁用);
-        let groupids = e.group_id;
-        let disable = data.groupids;
-        let group = data.groupids.disable;
-        if(group==null){group=[]};
-            if(e.msg.includes('本群禁用')){
-                let 功能 = e.msg.replace(/#|本群禁用|启用/g,'');
-                if(功能==group){return e.reply('该功能本群已禁用！请不要重复操作！')};
-                if(!功能){return e.reply('要禁用的功能呢？')};
-                group.push(功能);
-                await Yaml.getwrite(禁用, data);
-                let msgres = [segment.at(e.user_id), `【${e.msg}】已成功禁用！如要查看本群禁用功能可以使用指令【本群禁用列表】来查看哦~(如果禁用失败请重启试试)`];
-                await e.reply(msgres);
-                return false;
-            }
-
-            if(e.msg.includes('本群启用')){
-            let num = e.msg.match(/\d+/)
-                if (!num) {
-                return  e.reply('序号呢？不知道的话可以通过指令【本群禁用列表】来查看哦~')
-                }
-                let 恢复=group[num-1]
-                if(!恢复){return e.reply('请使用指令【本群禁用列表】，检查序号是否正确，或者检查【本群禁用列表】是否是空的！')}
-                await group.splice(group.indexOf(恢复), 1)
-                await Yaml.getwrite(禁用,data)
-                await e.reply('该功能已成功启用~(如果启用失败请重启试试)')
-            }
-    }
-
 };
+
+    function readPluginNames(directory) {
+    let pluginNames = [];
+    fs.readdirSync(directory).forEach(file => {
+    if (file.endsWith('.js')) {
+    pluginNames.push(file.slice(0, -3));
+    }
+    });
+    return pluginNames;
+}
