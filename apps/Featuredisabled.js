cb4e0dcd12286050223bd0d5ff0502ee4226e481
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-12 22:04:49 +0000

    update apps/Featuredisabled.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Featuredisabled.js
+++ apps/Featuredisabled.js
@@ -143,7 +143,7 @@ export class Featuredisabled extends plugin {
     ]
     let data=await Yaml.getread(禁用)
     let uid = e.group_id
-    data = uid;
+    data.push(uid);
     await Yaml.getwrite(禁用, data);
 
     let group = data.groupids.disable;
