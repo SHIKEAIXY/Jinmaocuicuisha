fa82516f1f9d99e29e3a675dd10ecd0b81a773d9
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-12 23:45:26 +0000

    update apps/Featuredisabled.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Featuredisabled.js
+++ apps/Featuredisabled.js
@@ -24,8 +24,16 @@ export class Featuredisabled extends plugin {
                     fnc: 'Ftdalllb',
                 },
                 {
-                    reg: '^#?(本群禁用列表)$',
-                    fnc: 'Ftdgrouplb',
+                    reg: '^#?(白名单列表)$',
+                    fnc: 'BFtdalllb',
+                },
+                {
+                    reg: '^#?(清空白名单)$',
+                    fnc: 'QBFtdall',
+                },
+                {
+                    reg: '^#?(设置|删除)白名单.*$',
+                    fnc: 'BFtdall',
                 },
                 {
                     reg: '^#?全局(禁用|启用).*$',
@@ -34,6 +42,101 @@ export class Featuredisabled extends plugin {
             ],
         });
     };
+
+    async QBFtdall(e) {
+
+    if (!e.isMaster) {e.reply(`你没有权限！`);return false;}
+    let mst = await Yaml.getread(path)
+    let 主人权限 = mst.绝对主人;
+    if (!(this.e.user_id==主人权限)){e.reply(`你没有权限！`);return false;}
+
+    if(e.msg.includes('#|清空白名单')){
+        let data=await Yaml.getread(禁用)
+        let All = data.default.enable;
+        All=[];
+        All.push()
+        await Yaml.getwrite(禁用,data)
+        return e.reply(`已清空白名单！`)
+        }
+    }
+
+    async BFtdalllb (e) {
+
+    if (!e.isMaster) {e.reply(`你没有权限！`);return false;}
+    let mst = await Yaml.getread(path)
+    let 主人权限 = mst.绝对主人;
+    if (!(this.e.user_id==主人权限)){e.reply(`你没有权限！`);return false;}
+
+    let nickname = Bot.nickname
+    if (this.e.isGroup) {
+      let info = await Bot.getGroupMemberInfo(this.e.group_id, Bot.uin)
+      nickname = info.card ?? info.nickname
+    }
+    let userInfo = {
+      user_id: Bot.uin,
+      nickname
+    }
+    let forwardMsg = [
+      {
+        ...userInfo,
+        message: '全局已禁用的功能\n可使用【全局启用+(序号)】启用对应功能'
+      }
+    ]
+    let data=await Yaml.getread(禁用)
+    let All = data.default.enable;
+    let msg=[]
+    logger.info(All)
+    if(All==null||All.length==0){return e.reply('没有禁用功能呢~')}
+    for (let v = 0; v < All.length; v++) {
+      msg.push(`${v+1}.`+All[v]+'\n')
+    }
+    forwardMsg.push(
+          {
+            ...userInfo,
+            message: msg
+          }
+        )
+    if (this.e.isGroup) {
+      forwardMsg = await this.e.group.makeForwardMsg(forwardMsg)
+    } else {
+      forwardMsg = await this.e.friend.makeForwardMsg(forwardMsg)
+    }
+    forwardMsg.data = forwardMsg.data
+      .replace(/\n/g, '')
+      .replace(/<title color="#777777" size="26">(.+?)<\/title>/g, '___')
+      .replace(/___+/, `<title color="#777777" size="26">全局已禁用的功能</title>`)
+    return e.reply(forwardMsg)
+    }
+
+    async BFtdall(e) {
+
+    if (!e.isMaster) {e.reply(`你没有权限！`);return false;}
+    let mst = await Yaml.getread(path)
+    let 主人权限 = mst.绝对主人;
+    if (!(this.e.user_id==主人权限)){e.reply(`你没有权限！`);return false;}
+
+    let data=await Yaml.getread(禁用)
+    let All = data.default.enable;
+        if(All==null){All=[]}
+            if(e.msg.includes('#|设置白名单')){
+            let 功能 = e.msg.replace(/#|设置|删除|白名单/g,'')
+            if(功能==All){return e.reply('该功能设置白名单！')}
+            if(!功能){return e.reply('要设置白名单的功能名字呢？')}
+            All.push(功能)
+            await Yaml.getwrite(禁用,data)
+            return e.reply(`功能【${e.msg}】设置白名单成功！`)
+            }
+
+            if(e.msg.includes('#|删除白名单')){
+            let num = e.msg.match(/\d+/)
+            if (!num) {return  e.reply('序号呢？不知道的话可以用指令【白名单列表】查看下功能对应的序号哦~')}
+            let 恢复=All[num-1]
+            if(!恢复){return e.reply('错误序号，请使用指令【白名单列表】查看一下序号是否正确，或者检查【白名单列表】是否为空！')}
+            await All.splice(All.indexOf(恢复), 1)
+            await Yaml.getwrite(禁用,data)
+            await e.reply('删除成功~')
+        }
+    }
 
     async Ftdalllb (e) {
 
@@ -90,56 +193,27 @@ export class Featuredisabled extends plugin {
     let 主人权限 = mst.绝对主人;
     if (!(this.e.user_id==主人权限)){e.reply(`你没有权限！`);return false;}
 
-    let nickname = Bot.nickname
-    if (this.e.isGroup) {
-      let info = await Bot.getGroupMemberInfo(this.e.group_id, Bot.uin)
-      nickname = info.card ?? info.nickname
-    }
-    let userInfo = {
-      user_id: Bot.uin,
-      nickname
-    }
-    let forwardMsg = [
-      {
-        ...userInfo,
-        message: '全局已禁用的功能\n可使用【全局启用+(序号)】启用对应功能'
-      }
-    ]
+    let data=await Yaml.getread(禁用)
+    let All = data.default.disable;
+        if(All==null){All=[]}
+            if(e.msg.includes('#|全局禁用')){
+            let 功能 = e.msg.replace(/#|全局|禁用|启用/g,'')
+            if(功能==All){return e.reply('该功能已禁用！')}
+            if(!功能){return e.reply('要禁用的功能名字呢？')}
+            All.push(功能)
+            await Yaml.getwrite(禁用,data)
+            return e.reply(`功能【${e.msg}】禁用成功！`)
+            }
 
-    let data = await readPluginNames(directory);
-    let All = data;
-    let msg=[]
-    logger.info(All)
-    if(All==null||All.length==0){return e.reply('没有禁用功能呢~')}
-    for (let v = 0; v < All.length; v++) {
-      msg.push(`${v+1}.`+All[v]+'\n')
+            if(e.msg.includes('#|全局启用')){
+            let num = e.msg.match(/\d+/)
+            if (!num) {return  e.reply('序号呢？不知道的话可以用指令【全局禁用列表】查看下功能对应的序号哦~')}
+            let 恢复=All[num-1]
+            if(!恢复){return e.reply('错误序号，请使用指令【全局禁用列表】查看一下序号是否正确，或者检查【全局禁用列表】是否为空！')}
+            await All.splice(All.indexOf(恢复), 1)
+            await Yaml.getwrite(禁用,data)
+            await e.reply('启用成功~')
+        }
     }
-    forwardMsg.push(
-          {
-            ...userInfo,
-            message: msg
-          }
-        )
-    if (this.e.isGroup) {
-      forwardMsg = await this.e.group.makeForwardMsg(forwardMsg)
-    } else {
-      forwardMsg = await this.e.friend.makeForwardMsg(forwardMsg)
-    }
-    forwardMsg.data = forwardMsg.data
-      .replace(/\n/g, '')
-      .replace(/<title color="#777777" size="26">(.+?)<\/title>/g, '___')
-      .replace(/___+/, `<title color="#777777" size="26">全局已禁用的功能</title>`)
-    return e.reply(forwardMsg)
-    }
-
 };
 
-    function readPluginNames(directory) {
-    let pluginNames = [];
-    fs.readdirSync(directory).forEach(file => {
-    if (file.endsWith('.js')) {
-    pluginNames.push(file.slice(0, -3));
-    }
-    });
-    return pluginNames;
-}
