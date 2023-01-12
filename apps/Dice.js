1d1dd2647adef43aa0ff84a2ec74613c4fe2c0b0
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-12 12:15:25 +0000

    update apps/Dice.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Dice.js
+++ apps/Dice.js
@@ -82,7 +82,7 @@ export class dice extends plugin {
     if (cd) return e.reply('每次只能开一局,请等这一局结束，或者发送【重置骰子】重新开始游戏。',true);
     cd = true;
     on = true;
-    let messageIds = [];
+    let messageIds = [0];
     let res = [segment.image(`file:///${images}/0.gif`),`\n请在【一】到【六】之间选一个数,然后发送【开】来查看结果！`];
     let roll = await e.reply(res)
     messageIds = roll;
@@ -137,7 +137,7 @@ export class dice extends plugin {
     if (!cd) return false;
     if (!en) return e.reply('还没有人猜呢~',true);
 
-        let messageIds = [];
+        let messageIds = [0];
         let roll = messageIds.pop();
         e.group.recallMsg(roll);
         
