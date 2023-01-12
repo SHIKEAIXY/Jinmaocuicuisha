a11acedda9bbba4a54c566333b657e87f68a77f2
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-12 22:19:14 +0000

    update apps/Featuredisabled.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Featuredisabled.js
+++ apps/Featuredisabled.js
@@ -142,7 +142,7 @@ export class Featuredisabled extends plugin {
       }
     ]
     let data=await Yaml.getread(禁用)
-    let group = data.e.group_id.disable;
+    let group = data.group_id.disable;
     let msg=[]
     logger.info(group)
     if(group==null||group.length==0){return e.reply('本群没有禁用功能呢~')}
