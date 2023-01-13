868c127da91826937cd90f9a6fdff770e5794029
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-13 12:58:15 +0000

    update apps/Admin.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Admin.js
+++ apps/Admin.js
@@ -210,7 +210,6 @@ async setblackGroup(e) {
     G = atItem[0].qq;
     }else{ G = G.match(/[1-9]\d*/g) }
     if (!G) return e.reply(`请输入正确的QQ群！`)
-    if (!(G==group_id)) return e.reply(`请输入正确的QQ群！`)
     G = parseInt(G);
     let TA = G;
 
@@ -384,7 +383,8 @@ async setMaster(e) {
     let TA = G;
 
     let 添加主人 = await Yaml.getread(主人);
-    if (!(TA==添加主人.masterQQ)){ e.reply(`该用户已经在主人列表了！请不要重复设置！`); return false;};
+    let A = 添加主人.masterQQ;
+    if ((TA==A)){ e.reply(`该用户已经在主人列表了！请不要重复设置！`); return false;};
     添加主人.masterQQ.push(TA);
     await Yaml.getwrite(主人, 添加主人);
     let msg = [segment.at(e.user_id), `已添加进主人列表！可以发送【主人列表】查看哦~`];
