75e687cf317a4efc63fa677aa0ed0024b43bc883
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-16 02:27:46 +0000

    update apps/Automaticwithdrawalset.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Automaticwithdrawalset.js
+++ apps/Automaticwithdrawalset.js
@@ -78,16 +78,16 @@ export class Automaticwithdrawalset extends plugin {
 
     if (!e.isMaster)  {return false}
      
-        let data1 = await Yaml.getread(Attl)
+        let data = await Yaml.getread(Attl)
         if (e.msg.includes('开启自动撤回')){
-        await data1.自动撤回 = true;
+        data.自动撤回 = true;
         await e.reply(`已开启自动撤回~`)
         }
         if (e.msg.includes('关闭自动撤回')){
-        await data1.自动撤回 = false;
+        data.自动撤回 = false;
         await e.reply(`已关闭自动撤回~`)
         }
-        Yaml.getwrite(Attl,data1)
+        Yaml.getwrite(Attl,data)
         return false;
     }
 
