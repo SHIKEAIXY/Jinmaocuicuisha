eba153653bad7ccbac2e37791b6b6d97cf9d400c
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-01-29 01:38:00 +0000

    update apps/Hitme.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Hitme.js
+++ apps/Hitme.js
@@ -323,8 +323,12 @@ async Hitta(e){
 
     if (e.atall){ e.reply(`${botname}打不过那么多人QAQ`); return true; }
     if (e.atme){ e.reply(`${botname}不能打自己！`); return true; }
-    if((e.isMaster == TA)){ e.reply(`${botname}不能打自己主人！`);return false}
-    
+    try {
+    let MasterQQ = await Yaml.getread(主人)
+    for (let qqq of MasterQQ) {
+    if(e.TA == qqq){e.reply(`${botname}不能打自己主人！`);return false}}
+    }catch (e){}
+
     let data = await redis.get(`dw:HitMe:${e.user_id}_cds`); 
     if (data) {
       console.log(data)
