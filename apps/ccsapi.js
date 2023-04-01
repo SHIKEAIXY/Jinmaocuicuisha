656fc0cfc2468eecccbf3fa91531bfcf8c985deb
小雨 <qw1314520po@outlook.com>
2023-04-01 03:36:44 +0000

    更新失效api和新添api
    Signed-off-by: 小雨 <qw1314520po@outlook.com>

--- apps/ccsapi.js
+++ apps/ccsapi.js
@@ -3,7 +3,7 @@ import plugin from "../../../lib/plugins/plugin.js"
 import fetch from 'node-fetch'
 
  
-let ciku = ["不想自己找api？使用以下动漫图片api全部可用\n动漫api一https://api.ixiaowai.cn/api/api.php\n动漫api二http://www.dmoe.cc/random.php\n动漫api三https://api.ixiaowai.cn/mcapi/mcapi.php\n动漫api四http://api.caonm.net/api/dm/index.php\n动漫api五https://img.paulzzh.tech/touhou/random\n动漫api六https://img.xjh.me/random_img.php?return=302\n动漫api七https://img.xjh.me/random_img.php\n动漫api八https://api.paugram.com/wallpaper/\n动漫api九https://api.mtyqx.cn/api/random.php\napi不正经的https://api.btstu.cn/sjbz/api.php\n动漫api十https://api.ixiaowai.cn/api/api2.php\n动漫api十一https://api.ixiaowai.cn/mcapi/mcapi2.php\n动漫api十二https://api.ixiaowai.cn/gqapi/gqapi2.php\n动漫api十三https://api.yimian.xyz/img\n使用方法 #写入打人api+上面的随便一个api即可 ----jinmocuicuisha-plugin"
+let ciku = ["不想自己找api？使用以下动漫图片api全部可用\n动漫api一https://api.ayao.ltd/PC/api.php\n动漫api二http://www.dmoe.cc/random.php\n动漫api三https://api.ayao.ltd/Mobile/api.php\n动漫api四http://api.caonm.net/api/dm/index.php\n动漫api五https://img.paulzzh.tech/touhou/random\n动漫api六https://img.xjh.me/random_img.php?return=302\n动漫api七https://img.xjh.me/random_img.php\n动漫api八https://api.paugram.com/wallpaper/\n动漫api九https://api.ayao.ltd/head-portrait/api.php \napi不正经的https://api.btstu.cn/sjbz/api.php\n动漫api十https://api.vvhan.com/api/acgimg\n动漫api十一http://www.dmoe.cc/random.php\n动漫api十二http://img.xjh.me/random_img.php\n动漫api十三https://api.yimian.xyz/img\n动漫api十四https://www.rrll.cc/tuceng/ecy.php\n动漫api十五https://api.ghser.com/random/api.php\n动漫api十六https://api.yimian.xyz/img \n使用方法 #写入打人api+上面的随便一个api即可 \n----jinmocuicuisha-plugin"
 ]
 
 export class ccsapi extends plugin {
