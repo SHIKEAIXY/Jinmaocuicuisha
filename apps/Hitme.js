e5ed5d93542001ea48fecb7a64cd3ad9cd209494
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-05-02 15:20:55 +0000

    update apps/Hitme.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/Hitme.js
+++ apps/Hitme.js
@@ -90,17 +90,16 @@ export class HitmeandTa extends plugin {
     }
 
     async Garbageccsapicklaile(e) {
-        const _path = process.cwd();
 
-        const text = "不想自己找api？使用以下动漫图片api全部可用\n动漫api一http://www.dmoe.cc/random.php\n动漫api二https://api.ayao.ltd/Mobile/api.php\n动漫api三http://api.caonm.net/api/dm/index.php\n动漫api四https://img.xjh.me/random_img.php?return=302\n动漫api五https://img.xjh.me/random_img.php\n动漫api六https://api.ayao.ltd/head-portrait/api.php \napi不正经的https://api.btstu.cn/sjbz/api.php\n动漫api七https://api.vvhan.com/api/acgimg\n动漫api八http://www.dmoe.cc/random.php\n动漫api九http://img.xjh.me/random_img.php\n动漫api十https://api.yimian.xyz/img\n动漫api十一https://api.ghser.com/random/api.php\n动漫api十二https://api.yimian.xyz/img \n使用方法 #写入打人api+上面的随便一个api即可 \n注意每次更换api时请先 #删除打人api 再写入api";
+        let text = "不想自己找api？使用以下动漫图片api全部可用\n动漫api一http://www.dmoe.cc/random.php\n动漫api二https://api.ayao.ltd/Mobile/api.php\n动漫api三http://api.caonm.net/api/dm/index.php\n动漫api四https://img.xjh.me/random_img.php?return=302\n动漫api五https://img.xjh.me/random_img.php\n动漫api六https://api.ayao.ltd/head-portrait/api.php \napi不正经的https://api.btstu.cn/sjbz/api.php\n动漫api七https://api.vvhan.com/api/acgimg\n动漫api八http://www.dmoe.cc/random.php\n动漫api九http://img.xjh.me/random_img.php\n动漫api十https://api.yimian.xyz/img\n动漫api十一https://api.ghser.com/random/api.php\n动漫api十二https://api.yimian.xyz/img \n使用方法 #写入打人api+上面的随便一个api即可 \n注意每次更换api时请先 #删除打人api 再写入api";
         let msg = [
             segment.at(e.user_id),
             text,
             // 返回一张图 （返回的图显示过期？请自行更换下面的api）
-            segment.image('http://api.caonm.net/api/dm/index.php')
+            segment.image(`http://api.caonm.net/api/dm/index.php`)
         ];
         e.reply(msg);
-        return true;
+        return false;
     }    
 
 async Hitmaster(e) {
