d970ea989572d87d5cd7aa24acad5d98850b815f
金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>
2023-06-18 16:33:43 +0000

    update apps/help.js.
    Signed-off-by: 金毛脆脆鲨 <12069514+JMCCS@user.noreply.gitee.com>

--- apps/help.js
+++ apps/help.js
@@ -50,41 +50,5 @@ export class Jmccshelp extends plugin {
 }
 
 async function help(e){
-  let custom = {}
-  let help = {}
-
-  let { diyCfg, sysCfg } = await Data.importCfg('help')
-
-  custom = help
-
-  let helpConfig = lodash.defaults(diyCfg.helpCfg || {}, custom.helpCfg, sysCfg.helpCfg)
-  let helpList = diyCfg.helpList || custom.helpList || sysCfg.helpList
-  let helpGroup = []
-
-  lodash.forEach(helpList, (group) => {
-    if (group.auth && group.auth === 'master' && !e.isMaster) {
-      return true
-    }
-
-    lodash.forEach(group.list, (help) => {
-      let icon = help.icon * 1
-      if (!icon) {
-        help.css = 'display:none'
-      } else {
-        let x = (icon - 1) % 10
-        let y = (icon - x - 1) / 10
-        help.css = `background-position:-${x * 50}px -${y * 50}px`
-      }
-    })
-
-    helpGroup.push(group)
-  })
-  let themeData = await Theme.getThemeData(diyCfg.helpCfg || {}, sysCfg.helpCfg || {})
-  
-  return await Common.render('help/index', {
-    helpCfg: helpConfig,
-    helpGroup,
-    ...themeData,
-    element: 'default'
-  }, { e, scale: 1 })
+    await e.runtime.render('Jinmaocuicuisha-plugin', '/help/help.html')
 }
